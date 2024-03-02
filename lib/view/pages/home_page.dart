c3fc57a0842f1dc4095554b2efa5a29783e19f9b
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-03-02 13:06:38 +0530

    Added CRUD operations

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:resume_builder_app/controller/resume_provider.dart';
+import 'package:resume_builder_app/view/pages/add_new_resume_page.dart';
+import 'package:resume_builder_app/view/pages/view_resume_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends ConsumerWidget {
@@ -15,6 +18,26 @@ class HomePage extends ConsumerWidget {
           .setBool('theme', themeNotifier.value);
     }
 
+    /// Add a new resume
+    void addNewResume() {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const AddNewResumePage(),
+          ));
+    }
+
+    /// View the resume
+    void viewResume(int index) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ViewResumePage(
+              index: index,
+            ),
+          ));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Quick Resume"),
@@ -26,10 +49,31 @@ class HomePage extends ConsumerWidget {
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      body: ref.watch(resumeProvider).isEmpty
+          ? const Center(
+              child: Text("Add a new resume"),
+            )
+          : ListView.separated(
+              itemBuilder: (context, index) {
+                final resume = ref.watch(resumeProvider)[index];
+
+                return ListTile(
+                  onTap: () => viewResume(index),
+                  title: Text(resume.name),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 16),
+              itemCount: ref.watch(resumeProvider).length,
+            ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => addNewResume(),
+        label: const Text(
+          "Add New Resume",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.teal,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
