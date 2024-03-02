41944cfe9929f90babcd20a39e64998177369516
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-03-02 11:42:09 +0530

    Added dependency , Theme
    Added shared_preference for storage and dark and light theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:resume_builder_app/view/pages/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /// State to store the theme data
+    final isDarkTheme = useState(false);
+
+    useEffect(() {
+      SharedPreferences.getInstance()
+          .then((value) => isDarkTheme.value = value.getBool('theme') ?? false);
+
+      return null;
+    });
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: isDarkTheme.value
+          ? ThemeData.dark()
+          : ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+              useMaterial3: true,
+            ),
       home: const HomePage(),
     );
   }
 }
-
